b67f6ab11cdf270ee93d1a57b375715f80a122d7
chstrAfk <chstrngt@gmail.com>
2025-10-24 13:46:55 -0400

    asa

---
+++ Pizza_Project/Pizza_JavaScript/Pizza.js
@@ -0,0 +1,53 @@
+function getReceipt() {
+// This initializes our string so it can get passed from
+// function to function, growing line by line into a full receipt
+var texti = "<h3>You Ordered: </h3>";
+runningTotal = 0;
+sizeArray = document.getElementsByClassName ("size");
+(var i = 0; i < sizeArray. length; i++) f
+if (sizeArray[i].checked) { 
+var selectedsize = sizeArray[i]-value;
+text1 = `${texti + selectedsize}‹br>`;
+}
+if (selectedsize === "Personal Pizza") {
+sizeTotal = 6;
+} else if (selectedSize === "Small Pizza") {
+sizeTotal = 8;
+} else if (selectedSize ==="Medium Pizza") {
+sizeTotal = 10;
+} else if (selectedSize ==="Large Pizza") {
+sizeTotal = 14;
+} else if (selectedSize ==="Extra Large Pizza") {
+sizeTotal = 16;
+}
+runningTotal = sizeTotal;
+console. log(selectedSize +"=$" + sizeTotal + ".00");
+console. log("size textl: " + textl);
+console. log("subtotal: $" + runningTotal + ".00");
+getTopping(runningTotal, text1); // These variables will be passed on to each function
+};
+function getTopping(runningTotal, text1) {
+var toppingTotal = 0;
+var selectedTopping = [];
+var toppingArray = document.getElementsByClassName("toppings");
+for (var j = 0; j< toppingArray.length; j++) {
+if (toppingArray[j]. checked) {
+selectedTopping.push(toppingArray[j].value);
+console.log("selected topping item: (" + toppingArray[j]. value + ")")
+text1 = text1 + toppingArrạy[j].value + "<br>";
+}
+}
+var toppingCount = selectedTopping. length;
+if (toppingCount > 1) {
+toppingTotal = (toppingCount - 1);
+} else {
+toppingTotal = 0;
+}
+runningTotal = (runningTotal + toppingTotal);
+console.log("total selected topping items: " + toppingcount);
+console.log(toppingCount + " topping" + "$" + toppingTotal + ".00");
+console.log("topping texti: " + texti);
+console.log("Purchase Total: " + "$" + runningTotal + ".00");
+document.getElementById("showText").innerHTML = text1;
+document.getElementById("totalPrice").innerHTML = "</h3>Total: <strong>$" + runningTotal + ".00" + "</strong></h3>";
+}
